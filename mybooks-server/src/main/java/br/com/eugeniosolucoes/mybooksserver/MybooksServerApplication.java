64980b46a09f70ba5eec4b4ab9e9d83d77849a17
eugeniosolucoes <ageniws@gmail.com>
2021-03-20 10:00:15 -0300

    Iniciando projeto mybooks server API

---
+++ mybooks-server/src/main/java/br/com/eugeniosolucoes/mybooksserver/MybooksServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.eugeniosolucoes.mybooksserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybooksServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybooksServerApplication.class, args);
+	}
+
+}
---
+++ mybooks-server/src/test/java/br/com/eugeniosolucoes/mybooksserver/MybooksServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eugeniosolucoes.mybooksserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybooksServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
