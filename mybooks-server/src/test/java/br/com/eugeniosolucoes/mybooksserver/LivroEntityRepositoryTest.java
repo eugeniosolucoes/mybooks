30e3a33dd0d06b38f98886dcdf550ba72b8bebee
eugeniosolucoes <ageniws@gmail.com>
2021-03-21 16:42:49 -0300

    Removendo testes com o banco.

--- mybooks-server/src/test/java/br/com/eugeniosolucoes/mybooksserver/LivroEntityRepositoryTest.java
+++ mybooks-server/src/test/java/br/com/eugeniosolucoes/mybooksserver/LivroEntityRepositoryTest.java
@@ -6,21 +6,14 @@
 package br.com.eugeniosolucoes.mybooksserver;
 
 import br.com.eugeniosolucoes.mybooks.MybooksServerApplication;
-import br.com.eugeniosolucoes.mybooks.dto.LivroDTO;
-import br.com.eugeniosolucoes.mybooks.model.Assunto;
-import br.com.eugeniosolucoes.mybooks.model.Autor;
-import br.com.eugeniosolucoes.mybooks.model.Livro;
 import br.com.eugeniosolucoes.mybooks.repository.LivroRepository;
 import br.com.eugeniosolucoes.mybooks.service.LivroService;
-import java.math.BigDecimal;
 import javax.persistence.EntityManager;
-import javax.transaction.Transactional;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
@@ -46,20 +39,4 @@ class LivroEntityRepositoryTest {
         assertThat( livroRepository ).isNotNull();
     }
 
-    @Test
-    @Transactional
-    @Rollback( false )
-    void incluirLivroAssuntoAutoresTest() {
-        Livro livro = new Livro( "Cangaceiro JavaScript", "Casa do Código", 1, "2021", new BigDecimal( "30.0" ) );
-        livro.getAssuntos().add( new Assunto( "Linguagem Javascript" ) );
-        livro.getAutores().add( new Autor( "Alexandre Eugênio" ) );
-        livroService.save( new LivroDTO( livro ) );
-    }
-
-    //@Test
-    @Transactional
-    @Rollback( false )
-    void excluirTodosTest() {
-        livroRepository.deleteAllInBatch();
-    }
 }
