d80ffe7b8fc9ddde67f69ef38bbc85517677efeb
eugeniosolucoes <ageniws@gmail.com>
2021-03-28 20:32:22 -0300

    Inclusão do componente chips do material design.

--- mybooks-app/src/app/livro/update-livro.component.ts
+++ mybooks-app/src/app/livro/update-livro.component.ts
@@ -6,6 +6,10 @@ import { LivroService } from './livro.service';
 
 import { FormLivro } from "./form-livro";
 
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { MatChipInputEvent } from '@angular/material/chips';
+
+
 @Component({
   selector: 'app-livro-update',
   templateUrl: './update-livro.component.html',
@@ -28,8 +32,6 @@ export class UpdateLivroComponent extends FormLivro implements OnInit {
   updateLivro(): void {
     try {
       this.livroService.tratarAnoEdicao(this.livro);
-      this.livroService.tratarAutores(this.livro);
-      this.livroService.tratarAssuntos(this.livro);
       this.livroService.updateLivro(this.livro)
         .subscribe(data => {
           alert("Livro atualizado com sucesso!");
@@ -39,5 +41,34 @@ export class UpdateLivroComponent extends FormLivro implements OnInit {
       alert(error);
     }
   };
+
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+
+  add(event: MatChipInputEvent, lista: any): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add assunto
+    if ((value || '').trim()) {
+      lista.push(value.trim());
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  remove(valor: string, lista: any): void {
+    const index = lista.indexOf(valor);
+
+    if (index >= 0) {
+      lista.splice(index, 1);
+    }
+  }
 
 }
